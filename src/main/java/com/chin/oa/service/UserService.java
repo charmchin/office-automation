8ced8e40bd0ed5e0868cd9262d633bc1ee38534b
charmchin <q15907285056k@163.com>
2024-01-29 15:54:57 +0800

    前端登录页面及逻辑实现

--- src/main/java/com/chin/oa/service/UserService.java
+++ src/main/java/com/chin/oa/service/UserService.java
@@ -4,14 +4,15 @@ import com.chin.oa.entity.User;
 import com.chin.oa.mapper.UserMapper;
 import com.chin.oa.service.exception.LoginException;
 
-public class UserService {
+public class UserService {//遵循MVC，写业务逻辑类
     private UserMapper userMapper = new UserMapper();
 
     /**
      * 根据前台输入进行登录校验
-     * @param username
+     * @param username 前台输入
      * @param password
      * @return校验通过后，包含对应用户数据的User实体类
+     * throws LoginException 自定义用户登录异常
      */
     public User checkLogin(String username , String password){
         User user = userMapper.selectByUsername(username);
--- src/main/java/com/chin/oa/service/exception/LoginException.java
+++ src/main/java/com/chin/oa/service/exception/LoginException.java
@@ -2,6 +2,6 @@ package com.chin.oa.service.exception;
 
 public class LoginException extends RuntimeException{
     public LoginException(String message) {
-        super(message);
+        super(message);//直接调用父类的默认逻辑就可以了
     }
 }
